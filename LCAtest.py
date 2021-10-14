c695a9451752f42646acd4503df9073bce49ea73
doimiod <91431813+doimiod@users.noreply.github.com>
2021-10-14 23:22:22 +0100

    Add files via upload

---
+++ LCAtest.py
@@ -0,0 +1,26 @@
+import unittest
+import LCA
+
+
+class testLCA(unittest.TestCase):
+
+
+    def setUP(self):
+        self.root = LCA.root = LCA.Node(1)
+        self.root.left = LCA.Node(2)
+        self.root.right = LCA.Node(3)
+        self.root.left.left = LCA.Node(4)
+        self.root.left.right = LCA.Node(5)
+        self.root.right.left = LCA.Node(6)
+        self.root.right.right = LCA.Node(7)
+        self.root.left.left.left = LCA.Node(8)
+        self.root.left.left.right = LCA.Node(9)
+
+
+    def test(self):
+        self.assertTrue(LCA.findLCA(LCA.root, 2, 9), 1)
+        self.assertTrue(LCA.findLCA(LCA.root, 5, 7), 1)
+        
+
+        if __name__ == '__main__':
+            unittest.main()
